182ef0c2d8911cf8aadad3cb8591e6b1e5ef6c98
Nirmani Waduressa <nirmaniakalanka129@gmail.com>
2026-04-19 13:09:11 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var btn=document.getElementById("btn")
+btn.onclick=function(){
+    alert("Message sent successfully!")
+};
+var i=document.getElementById("icon.linkedin")
+i.onclick=function(){
+    window.open("https://www.linkedin.com/in/nirmani-waduressa-86b95a242", "_blank")
+};
+var blogbtn=document.getElementById("view-blog-btn")
+blogbtn.onclick=function(){
+    window.open("https://medium.com/@nirmaniakalanka129/disneys-principles-of-animation-d1085ebddd12", "_blank")
+};
+var menuIcon=document.getElementById("menu-icon")
+var navLinks=document.getElementById("nav-links")
+menuIcon.onclick=function(){
+    navLinks.classList.toggle("active")
+};
+var visitbtn=document.getElementById("visit-github-btn")
+visitbtn.onclick=function(){
+    window.open("https://github.com/nirmaniakalanka129-ux/Personal-Portfolio", "_blank")
+};
+var igithub=document.getElementById("icon.github")
+igithub.onclick=function(){
+    window.open("https://github.com/nirmaniakalanka129-ux", "_blank")
+};  
